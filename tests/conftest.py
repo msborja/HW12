da3b3b76b579f18eb71731f82cac0a14350e0f5a
MBorja <chealsy25@gmail.com>
2024-06-12 17:41:44 +0300

    Do Homework

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,19 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -8,5 +22,23 @@ def browser_settings():
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 10.0
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     yield
     browser.quit()
