866d11cf964a3acf0dc78cd199f7e7777ed7fe68
MBorja <chealsy25@gmail.com>
2024-06-13 16:10:06 +0300

    Fix

--- utils/attach.py
+++ utils/attach.py
@@ -1,5 +1,6 @@
 import allure
 from allure_commons.types import AttachmentType
+from dotenv import load_dotenv
 
 
 # Скриншоты
@@ -22,7 +23,7 @@ def add_html(browser):
 
 # скринкаст
 def add_video(browser):
-    video_url = f"{selenoid_url}/video/" + browser.driver.session_id + ".mp4"
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
